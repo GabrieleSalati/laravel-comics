b249db54119089abc39868cc5a0b9cffef6a0ec4
GabrieleSalati <gabriele.salati5@gmail.com>
2023-03-29 19:39:14 +0200

    database linked to home ok

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ env("APP_NAME") }} - Home</title>
+
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <h1>ciao</h1>
+
+    @dump($comics)
+</body>
+
+</html>
